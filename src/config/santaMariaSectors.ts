05e3911a4c486bb717023cc4109b4cc04595ab9d
Sergio Tezzei <sergiotezzei@gmail.com>
2026-08-05 16:54:57 -0300

    Centraliza setores dos chamados

---
+++ src/config/santaMariaSectors.ts
@@ -0,0 +1,21 @@
+export const santaMariaRequestSectors = [
+  "Administração",
+  "Diretoria",
+  "Infraestrutura",
+  "Manutenção",
+  "Limpeza",
+  "Copa / Café",
+  "Segurança",
+  "Recepção",
+  "Financeiro",
+  "Contratos",
+  "Locação",
+  "Vendas",
+  "Marketing",
+  "Jurídico",
+  "Compras / Estoque",
+  "Patrimônio",
+  "Outro",
+] as const;
+
+export type SantaMariaRequestSector = (typeof santaMariaRequestSectors)[number];
